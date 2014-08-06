6648edd20f3da9966b1438438baad7a760880458
onqtam <vik.kirilov@gmail.com>
2014-08-07 01:52:04 +0300

    - made it possible to disable callback registration with a define

--- doctest.h
+++ doctest.h
@@ -7,7 +7,6 @@
 //   5k doctests in 1 cpp ==  9 sec build time on my machine
 // 2.5k doctests in 1 cpp ==  3 sec build time on my machine
 // == TODO:
-// - disabling function registration with a macro
 // - think about in-class doctests
 // - macro for making the lib not header only - for multi dll scenarios
 //   (will force the user to use a macro in only 1 dll in only 1 source file)
@@ -17,7 +16,10 @@
 // - reporting
 // - documentation
 
+#if !defined(DOCTEST_GLOBAL_DISABLE)
+
 #if !defined(DOCTEST_NOT_HEADER_ONLY)
+// required includes
 #include <map>
 #include <cstring>
 #endif // DOCTEST_NOT_HEADER_ONLY)
@@ -143,10 +145,15 @@ int r(functionType f, unsigned line, const char* file, const char* method, const
 void invokeAllFunctions(int argc, char** argv);
 #endif // DOCTEST_NOT_HEADER_ONLY
 
+} // namespace doctestns
+
+
+
 // ============================================================================
-// internal macros
+// macros for creating and registering test functions - produce optimal codegen
 // ============================================================================
 
+// internal macros
 #define DOCTEST_STR_CONCAT_IMPL(s1, s2) s1##s2
 #define DOCTEST_STR_CONCAT(s1, s2) DOCTEST_STR_CONCAT_IMPL(s1, s2)
 #if defined(__COUNTER__) // not standard and may be missing for some compilers
@@ -155,23 +162,20 @@ void invokeAllFunctions(int argc, char** argv);
 #define DOCTEST_ANONYMOUS_NAME(x) DOCTEST_STR_CONCAT(x, __LINE__)
 #endif
 
-// ============================================================================
-// macros for creating and registering test functions - produce optimal codegen
-// ============================================================================
-
+// internal registering macros
 #define DOCTEST_REGISTER_FUNCTION(f, name) \
 static int DOCTEST_ANONYMOUS_NAME(a)=r(f,__LINE__,__FILE__,"",#name);
 
 #define DOCTEST_REGISTER_CLASS_FUNCTION(x, m) \
 namespace doctestns{static int DOCTEST_ANONYMOUS_NAME(a)=r(&x::m,0,__FILE__,#m,"");}
 
-#define DOCTEST_CREATE_AND_REGISTER_FUNCTION(f, name) \
-namespace doctestns{void f();DOCTEST_REGISTER_FUNCTION(f, name)}inline void doctestns::f()
-
 #define DOCTEST_IMPLEMENT_FIXTURE(der, base, func, name) \
 namespace doctestns{struct der:base{void f();};inline void func(){der v;v.f();}\
 static int DOCTEST_ANONYMOUS_NAME(a)=r(func,__LINE__,__FILE__,"",#name);}\
 inline void doctestns::der::f()
+
+#define DOCTEST_CREATE_AND_REGISTER_FUNCTION(f, name) \
+namespace doctestns{void f();DOCTEST_REGISTER_FUNCTION(f, name)}inline void doctestns::f()
 
 // for registering normal doctests
 #define doctest(name) \
@@ -181,6 +185,38 @@ DOCTEST_CREATE_AND_REGISTER_FUNCTION(DOCTEST_ANONYMOUS_NAME(f), name)
 #define doctest_fixture(x, name) \
 DOCTEST_IMPLEMENT_FIXTURE(DOCTEST_ANONYMOUS_NAME(F), x, DOCTEST_ANONYMOUS_NAME(f), name)
 
-} // namespace doctestns
+// =============================================================================
+// == WHAT FOLLOWS IS VERSIONS OF THE MACROS THAT DO NOT DO ANY REGISTERING!  ==
+// == THIS CAN BE ENABLED BY DEFINING DOCTEST_GLOBAL_DISABLE GLOBALLY!        ==
+// =============================================================================
+#else // DOCTEST_GLOBAL_DISABLE
+
+#define DOCTEST_STR_CONCAT_IMPL(s1, s2) s1##s2
+#define DOCTEST_STR_CONCAT(s1, s2) DOCTEST_STR_CONCAT_IMPL(s1, s2)
+#if defined(__COUNTER__) // not standard and may be missing for some compilers
+#define DOCTEST_ANONYMOUS_NAME(x) DOCTEST_STR_CONCAT(x, __COUNTER__)
+#else
+#define DOCTEST_ANONYMOUS_NAME(x) DOCTEST_STR_CONCAT(x, __LINE__)
+#endif
+
+#define DOCTEST_INVOKE_ALL_TEST_FUNCTIONS(argc, argv)
+#define DOCTEST_REGISTER_FUNCTION(f, name)
+#define DOCTEST_REGISTER_CLASS_FUNCTION(x, m)
+#define DOCTEST_IMPLEMENT_FIXTURE(der, base, func, name) \
+namespace doctestns{struct der:base{void f();};inline void func(){der v;v.f();}}\
+inline void doctestns::der::f()
+
+#define DOCTEST_CREATE_AND_REGISTER_FUNCTION(f, name) \
+namespace doctestns{void f();DOCTEST_REGISTER_FUNCTION(f, name)}inline void doctestns::f()
+
+// for registering normal doctests
+#define doctest(name) \
+DOCTEST_CREATE_AND_REGISTER_FUNCTION(DOCTEST_ANONYMOUS_NAME(f), name)
+
+// for registering doctests with a fixture
+#define doctest_fixture(x, name) \
+DOCTEST_IMPLEMENT_FIXTURE(DOCTEST_ANONYMOUS_NAME(F), x, DOCTEST_ANONYMOUS_NAME(f), name)
+
+#endif // DOCTEST_GLOBAL_DISABLE
 
 #endif // __DOCTEST_HEADER_INCLUDED__
--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
 #include "doctest.h"
 
+#include <cstdio>
+
+doctest(Asd) { printf("%s\n", __func__); }
+doctest() { printf("%s\n", __func__); }
 doctest(Asd) { printf("%s\n", __func__); }
 doctest(asdfa) { printf("%s\n", __func__); }
 doctest(Asd) { printf("%s\n", __func__); }
 doctest(adffd) { printf("%s\n", __func__); }
 doctest(qweqq) { printf("%s\n", __func__); }
+doctest() { printf("%s\n", __func__); }
 
 struct MILF{};
 doctest_fixture(MILF, ) { printf("%s\n", __func__); }
 doctest_fixture(MILF, qwe) { printf("%s\n", __func__); }
 doctest_fixture(MILF, q) { printf("%s\n", __func__); }
 doctest_fixture(MILF, dsf) { printf("%s\n", __func__); }
+
