fdd998072f8a6cdec6724216843107275228b192
onqtam <vik.kirilov@gmail.com>
2014-12-16 12:21:02 +0200

    tiny

--- examples/multiprocess/run.py
+++ examples/multiprocess/run.py
@@ -20,7 +20,7 @@ data = [l[i : i + n] for i in range(1, len( l ), n)]
 data = tuple([[x[0], x[-1]] for x in data])
 
 # for 8 cores and 100 tests the ranges will look like this
-# [1, 13], [14, 26], [27, 39], [40, 52], [53, 65], [66, 78], [79, 91], [92, 100])
+# ([1, 13], [14, 26], [27, 39], [40, 52], [53, 65], [66, 78], [79, 91], [92, 100])
 
 # the worker callback that runs the executable for the given range of tests
 def worker((first, last)):
