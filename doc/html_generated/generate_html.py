159c0334b35715b8ac4cb77a34bcbc81a815834a
onqtam <vik.kirilov@gmail.com>
2015-01-25 12:56:45 +0200

    regenerated docs!

--- doc/html_generated/generate_html.py
+++ doc/html_generated/generate_html.py
@@ -14,7 +14,7 @@ for filename in os.listdir('./../'):
         html.write('<xmp theme="united" style="display:none;">\n\n')
         md_contents = md_contents.replace(".md)", ".html)")
         html.write(md_contents)
-        html.write('\n</xmp>\n')
+        html.write('\n\n</xmp>\n')
         html.write('<script src="strapdown.js/strapdown.js"></script>\n')
         html.write('</html>\n')
         html.close()
