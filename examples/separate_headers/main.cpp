bc4b56c5d642ae070af58536911c68d770b9cd72
onqtam <vik.kirilov@gmail.com>
2016-05-31 18:24:00 +0300

    fixed #10
    also added an example using the parts of the doctest header (and fixed it to work)

---
+++ examples/separate_headers/main.cpp
@@ -0,0 +1,21 @@
+#include "parts/doctest_impl.h"
+
+int main(int argc, char** argv) {
+    doctest::Context context(argc, argv); // initialize
+
+    // overrides
+    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in the name
+    context.setOption("no-breaks", true); // don't break in the debugger when assertions fail
+    context.setOption("abort-after", 5);  // stop test execution after 5 failed assertions
+    context.setOption("sort", "name");    // sort the test cases by their name
+
+    int res = context.run(); // run
+
+    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+        return res;          // propagate the result of the tests
+
+    int client_stuff_return_code = 0;
+    // your program - if the testing framework is integrated in your production code
+
+    return res + client_stuff_return_code;
+}
