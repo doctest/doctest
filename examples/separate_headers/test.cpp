c4ff1cacf61933b6326b20c967af18b9132046d8
onqtam <vik.kirilov@gmail.com>
2016-06-08 16:19:38 +0300

    related to #14 - added a header with a specialized macro for equality
    removed WARN assertions from the assertion count

--- examples/separate_headers/test.cpp
+++ examples/separate_headers/test.cpp
@@ -1,5 +1,10 @@
 #include "parts/doctest_fwd.h"
+#include "parts/doctest_fast.h"
 
 TEST_CASE("captain obvious") {
-    CHECK(true == false);
+    //CHECK(true == false);
+
+    WARN_EQ(false, true);
+    CHECK_EQ(false, true);
+    REQUIRE_EQ(false, true);
 }
