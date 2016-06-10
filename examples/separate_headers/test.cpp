8def6b040fd176f929e9ebc881810745e11e0a2d
onqtam <vik.kirilov@gmail.com>
2016-06-10 21:28:08 +0300

    removed custom implementations of vector and hashtable and now using std::vector and std::set

--- examples/separate_headers/test.cpp
+++ examples/separate_headers/test.cpp
@@ -1,12 +1,31 @@
 #include "parts/doctest_fwd.h"
 
-TEST_CASE("captain obvious") {
-    bool b = false;
-    CHECK(b);
+#include <cstdio>
 
-    CHECK_UNARY_FALSE(true);
+TEST_CASE("custom macros") {
+    printf("Hello world!\n");
 
-    WARN_GT(false, false);
-    CHECK_EQ(false, true);
-    REQUIRE_EQ(false, true);
+    CHECK(1 == 1);
+
+    SUBCASE("bar") {
+        SUBCASE("foo") {
+            printf("foo\n"); // 
+        }
+        SUBCASE("baz") {
+            printf("baz\n"); // 
+        }
+
+        printf("end\n"); // 
+    }
 }
+
+//TEST_CASE("captain obvious") {
+//    bool b = false;
+//    CHECK(b);
+//
+//    CHECK_UNARY_FALSE(true);
+//
+//    WARN_GT(false, false);
+//    CHECK_EQ(false, true);
+//    REQUIRE_EQ(false, true);
+//}
