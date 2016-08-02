4321fd808ce77938e1f0862ebba8159dc055aeaf
onqtam <vik.kirilov@gmail.com>
2016-08-02 14:59:17 +0300

    more coverage

--- examples/code_coverage/main.cpp
+++ examples/code_coverage/main.cpp
@@ -1,26 +1,23 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 
-int main(int argc, char** argv) {
-    doctest::Context context; // initialize
+TEST_CASE("throws") {
+    CHECK(1 == 2);
+}
 
-    // defaults
-    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in the name
-    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
+int main(int argc, char** argv) {
+    doctest::Context context;
+
+    context.addFilter("test-case-exclude", "*math*");
+    context.setOption("no-breaks", true);
+    context.setOption("sort", "name");
 
     context.applyCommandLine(argc, argv);
 
-    // overrides
-    context.setOption("abort-after", 5); // stop test execution after 5 failed assertions
-    context.setOption("sort", "name");   // sort the test cases by their name
+    int res = context.run();
 
-    int res = context.run(); // run
+    if(context.shouldExit())
+        return res;
 
-    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-        return res;          // propagate the result of the tests
-
-    int client_stuff_return_code = 0;
-    // your program - if the testing framework is integrated in your production code
-
-    return res + client_stuff_return_code;
+    return res;
 }
--- examples/code_coverage/test.cpp
+++ examples/code_coverage/test.cpp
@@ -10,15 +10,34 @@ static int throws(bool in) {
 
 TEST_SUITE("ts1");
 
-TEST_CASE("ops") {
-    throws(false);
+TEST_CASE("assertions") {
+    int iVar = 0;
+    int* a = &iVar;
+    int* b = a - 1;
+    CHECK(a == b);
+    CHECK(1 == false);
+    CHECK(Approx(0.1) == 0.2);
 
+    CHECK_THROWS(throws(true));
+    CHECK_NOTHROW(throws(false));
+    CHECK_THROWS_AS(throws(true), bool);
+}
 
-
+TEST_CASE("throws") {
+    throws(true);
 }
 
 TEST_SUITE_END();
 
-
 TEST_SUITE("ts2");
 
+TEST_CASE("subcases") {
+    SUBCASE("1") {
+        SUBCASE("1.1") {}
+        SUBCASE("1.2") {}
+    }
+    SUBCASE("2") {}
+    SUBCASE("3") {}
+}
+
+TEST_SUITE_END();
