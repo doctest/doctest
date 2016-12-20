7d000e47fdace395c3c993787ec98e3a06bbe6b2
Elias Kosunen <elias.kosunen@gmail.com>
2016-12-20 18:57:03 +0200

    Add assertion macros regarding std::exception handling to examples/ directory

--- examples/assertion_macros/main.cpp
+++ examples/assertion_macros/main.cpp
@@ -7,6 +7,12 @@ static int throws(bool in) {
     return 42;
 }
 
+static int throws_stdexcept(bool in) {
+    if(in)
+        throw std::runtime_error("Exception");
+    return 42;
+}
+
 using doctest::Approx;
 
 TEST_SUITE("meaningless macros");
@@ -14,6 +20,8 @@ TEST_SUITE("meaningless macros");
 TEST_CASE("an empty test that will succeed") {}
 
 TEST_CASE("an empty test that will fail because of an exception") { throws(true); }
+
+TEST_CASE("an empty test that will fail because of a std::exception") { throws_stdexcept(true); }
 
 TEST_SUITE_END();
 
@@ -40,11 +48,43 @@ TEST_CASE("normal macros") {
     throws(true);
 }
 
+TEST_CASE("normal macros with std::exception") {
+    int a = 5;
+    int b = 5;
+
+    CHECK(throws_stdexcept(true) == 42);
+
+    CHECK_FALSE(!(a == b));
+
+    REQUIRE(a == b);
+
+    CHECK_EQ(a, b);
+
+    FAST_CHECK_EQ(a, b);
+
+    // commented out because 32 vs 64 bit builds will fail when the output is compared
+    //WARN(reinterpret_cast<void*>(1000) == reinterpret_cast<void*>(1004));
+
+    CHECK(Approx(0.1000001) == 0.1000002);
+    CHECK(Approx(0.502) == 0.501);
+
+    throws(true);
+}
+
 TEST_CASE("exceptions-related macros") {
     CHECK_THROWS(throws(false));
     CHECK_THROWS_AS(throws(false), int);
     CHECK_THROWS_AS(throws(true), int);
     CHECK_THROWS_AS(throws(true), char);
 
-    REQUIRE_NOTHROW(throws(true));
+    CHECK_NOTHROW(throws(true));
+}
+
+TEST_CASE("exceptions-related macros for std::exception") {
+    CHECK_THROWS(throws_stdexcept(false));
+    CHECK_THROWS_AS(throws_stdexcept(false), std::exception);
+    CHECK_THROWS_AS(throws_stdexcept(true), std::exception);
+    CHECK_THROWS_AS(throws_stdexcept(true), int);
+
+    REQUIRE_NOTHROW(throws_stdexcept(true));
 }
