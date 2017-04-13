6abb354fb5a1d1366cd7af94f4750223682e0b59
onqtam <vik.kirilov@gmail.com>
2017-04-13 19:16:37 +0300

    fixing -Wunused-parameter warning introduced in last commit

--- doctest/parts/doctest_fwd.h
+++ doctest/parts/doctest_fwd.h
@@ -1549,6 +1549,7 @@ namespace detail
                 return true;
             } catch(...) {}
 #endif // DOCTEST_CONFIG_NO_EXCEPTIONS
+            ((void)res); // to silence -Wunused-parameter
             return false;
         }
 
