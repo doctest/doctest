5b3264eab2637d6f08055f7ab612f7bacbcfaac7
onqtam <vik.kirilov@gmail.com>
2017-04-16 22:11:21 +0300

    cleaning up examples

--- examples/all_features/assertion_macros.cpp
+++ examples/all_features/assertion_macros.cpp
@@ -1,4 +1,3 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
 template<typename T>
--- examples/multi_file_example/hdr.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "doctest.h"
-
-// this test will be registered only once even though it is in a header file
-TEST_CASE("hdr.h") {
-    CHECK(1 == 0);
-}
-
