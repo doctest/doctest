527a8699594f232861e7645fe935f6b576c92fd2
onqtam <vik.kirilov@gmail.com>
2017-04-16 23:21:13 +0300

    output is now like the old one

--- examples/all_features/main.cpp
+++ examples/all_features/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char** argv) {
 
     // defaults
     context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in the name
-    context.setOption("abort-after", 5);   // stop test execution after 5 failed assertions
-    context.setOption("order-by", "name"); // sort the test cases by their name
+    //context.setOption("abort-after", 5);   // stop test execution after 5 failed assertions
+    context.setOption("order-by", "file"); // sort the test cases by file and line
 
     context.applyCommandLine(argc, argv);
 
