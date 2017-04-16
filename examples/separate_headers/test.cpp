bc859767bbf066905d623391c4c88775443002a9
onqtam <vik.kirilov@gmail.com>
2017-04-16 09:39:01 +0300

    - removed warning that is included in Wall/Wextra (although not documented in the online docs of GCC) - removed old unnecessary stuff

--- examples/separate_headers/test.cpp
+++ examples/separate_headers/test.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 using namespace std;
 
-TEST_CASE("dev stuff") {
+//static int throws(bool in) { if(in) throw 42; return 0; }
 
+TEST_CASE("dev stuff") {
+    CHECK(4 == 5);
+    //CHECK_THROWS(CHECK_THROWS(4 == 5));
+    //CHECK_THROWS({throws(true);});
 }
