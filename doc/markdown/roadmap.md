88d3e8670b9c4c43f889cda936596caed3b7a6ec
onqtam <vik.kirilov@gmail.com>
2017-05-29 23:38:14 +0300

    code coverage maxed

--- doc/markdown/roadmap.md
+++ doc/markdown/roadmap.md
@@ -61,6 +61,7 @@ Planned features for future releases - order changes constantly...
     - should test stuff that should not compile
     - should test crash handling
     - should test more config options
+    - don't cheat for maxing out code coverage (see [coverage_maxout.cpp](../../examples/coverage_maxout.cpp)
     - should test C++11 stuff - perhaps inspect the CMAKE_CXX_FLAGS for -std=c++11 on the CI and add more targets/tests
     - test tricky stuff like expressions with commas in asserts
 
--- examples/all_features/coverage_maxout.cpp
+++ examples/all_features/coverage_maxout.cpp
@@ -19,6 +19,7 @@ namespace detail
     const char* fileForOutput(const char* file);
     void reportFatal(const std::string&);
     int wildcmp(const char* str, const char* wild, bool caseSensitive);
+    void myOutputDebugString(const String&);
 } // namespace detail
 } // namespace doctest
 
@@ -26,6 +27,7 @@ TEST_CASE("doctest internals") {
     using namespace doctest;
     detail::reportFatal("");
     detail::wildcmp("str", "str*", false);
+    detail::myOutputDebugString("");
 
     // trigger code path for comparing the file in "operator<" of SubcaseSignature
     CHECK(detail::SubcaseSignature("", "a.cpp", 0) < detail::SubcaseSignature("", "b.cpp", 0));
@@ -43,6 +45,7 @@ TEST_CASE("doctest internals") {
     // toString
     a += toString("aaa")                           //
          + toString(true)                          //
+         + toString(static_cast<unsigned int>(0))  //
          + toString(0.5f)                          //
          + toString(0.5)                           //
          + toString(static_cast<long double>(0.1)) //
@@ -60,6 +63,20 @@ TEST_CASE("doctest internals") {
     a += detail::fileForOutput("a");
 
     std::ostringstream oss;
+
+    // toString
+    detail::toStream(&oss, true);
+    detail::toStream(&oss, 0.5f);
+    detail::toStream(&oss, 0.5);
+    detail::toStream(&oss, static_cast<long double>(0.1));
+    detail::toStream(&oss, 'c');
+    detail::toStream(&oss, static_cast<signed char>('c'));
+    detail::toStream(&oss, static_cast<unsigned char>(1));
+    detail::toStream(&oss, static_cast<short>(1));
+    detail::toStream(&oss, static_cast<long>(1));
+    detail::toStream(&oss, static_cast<unsigned long>(1));
+    detail::toStream(&oss, static_cast<unsigned short>(1));
+
     // trigger code path for String to ostream through operator<<
     oss << a;
     // trigger code path for assert string of a non-existent assert type
