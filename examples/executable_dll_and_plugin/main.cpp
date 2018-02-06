8a0a0235dd0a9a69dc34e5b074869eded05524ac
onqtam <vik.kirilov@gmail.com>
2018-02-06 22:57:12 +0200

    fixed indentation which led to a gcc warning

--- examples/executable_dll_and_plugin/main.cpp
+++ examples/executable_dll_and_plugin/main.cpp
@@ -57,8 +57,8 @@ int main(int argc, char** argv) {
     if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
         return res;          // propagate the result of the tests
 
-	int client_stuff_return_code = 0;
-	// your program - if the testing framework is integrated in your production code
+    int client_stuff_return_code = 0;
+    // your program - if the testing framework is integrated in your production code
 
     return res + client_stuff_return_code; // the result from doctest is propagated here as well
 }
