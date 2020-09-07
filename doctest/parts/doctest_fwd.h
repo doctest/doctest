460afe9325a1dee07c976dcd2735e1db5d09ab46
Viktor Kirilov <vik.kirilov@gmail.com>
2020-09-07 12:19:06 +0300

    updated license years

--- doctest/parts/doctest_fwd.h
+++ doctest/parts/doctest_fwd.h
@@ -1,7 +1,7 @@
 //
 // doctest.h - the lightest feature-rich C++ single-header testing framework for unit tests and TDD
 //
-// Copyright (c) 2016-2019 Viktor Kirilov
+// Copyright (c) 2016-2020 Viktor Kirilov
 //
 // Distributed under the MIT Software License
 // See accompanying file LICENSE.txt or copy at
