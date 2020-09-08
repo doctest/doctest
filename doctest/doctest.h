6f24dfeacd62e6eff4a1c1fece836b1e50b41870
Viktor Kirilov <vik.kirilov@gmail.com>
2020-09-08 15:27:51 +0300

    pointing to the relevant part of Catch2 for the debugger detection

--- doctest/doctest.h
+++ doctest/doctest.h
@@ -3854,33 +3854,22 @@ namespace detail {
 #ifdef DOCTEST_PLATFORM_LINUX
     class ErrnoGuard {
     public:
-        ErrnoGuard():m_oldErrno(errno){}
+        ErrnoGuard() : m_oldErrno(errno) {}
         ~ErrnoGuard() { errno = m_oldErrno; }
     private:
         int m_oldErrno;
     };
-    // The standard POSIX way of detecting a debugger is to attempt to
-    // ptrace() the process, but this needs to be done from a child and not
-    // this process itself to still allow attaching to this process later
-    // if wanted, so is rather heavy. Under Linux we have the PID of the
-    // "debugger" (which doesn't need to be gdb, of course, it could also
-    // be strace, for example) in /proc/$PID/status, so just get it from
-    // there instead.
+    // See the comments in Catch2 for the reasoning behind this implementation:
+    // https://github.com/catchorg/Catch2/blob/v2.13.1/include/internal/catch_debugger.cpp#L79-L102
     bool isDebuggerActive() {
-        // Libstdc++ has a bug, where std::ifstream sets errno to 0
-        // This way our users can properly assert over errno values
         ErrnoGuard guard;
         std::ifstream in("/proc/self/status");
-        for( std::string line; std::getline(in, line); ) {
+        for(std::string line; std::getline(in, line);) {
             static const int PREFIX_LEN = 11;
-            if( line.compare(0, PREFIX_LEN, "TracerPid:\t") == 0 ) {
-                // We're traced if the PID is not 0 and no other PID starts
-                // with 0 digit, so it's enough to check for just a single
-                // character.
+            if(line.compare(0, PREFIX_LEN, "TracerPid:\t") == 0) {
                 return line.length() > PREFIX_LEN && line[PREFIX_LEN] != '0';
             }
         }
-
         return false;
     }
 #elif defined(DOCTEST_PLATFORM_MAC)
