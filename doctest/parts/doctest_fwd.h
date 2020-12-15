2a069c43af43973c1923c6838d0dbe48ee3e5f84
onqtam <vik.kirilov@gmail.com>
2020-12-15 21:02:28 +0200

    version 2.4.2

--- doctest/parts/doctest_fwd.h
+++ doctest/parts/doctest_fwd.h
@@ -45,8 +45,8 @@
 
 #define DOCTEST_VERSION_MAJOR 2
 #define DOCTEST_VERSION_MINOR 4
-#define DOCTEST_VERSION_PATCH 1
-#define DOCTEST_VERSION_STR "2.4.1"
+#define DOCTEST_VERSION_PATCH 2
+#define DOCTEST_VERSION_STR "2.4.2"
 
 #define DOCTEST_VERSION                                                                            \
     (DOCTEST_VERSION_MAJOR * 10000 + DOCTEST_VERSION_MINOR * 100 + DOCTEST_VERSION_PATCH)
