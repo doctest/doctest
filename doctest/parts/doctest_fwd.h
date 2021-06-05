8131d3fc91abb9679f372e34b248c0a6cb59ab1d
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2021-06-05 11:03:30 -0700

    Fix 'ignoring return value' warning with gcc 9.3
    Without this fix, GCC 9.3.0 generates the following warning when
    comparing shared_ptr to nullptr:
    
    extern/doctest.h:1239:49: error: ignoring return value of ‘bool std::operator!=(const std::shared_ptr<_Tp>&, std::nullptr_t) [with _Tp = Luau::Module; std::nullptr_t = std::nullptr_t]’, declared with attribute nodiscard [-Werror=unused-result]
     1239 |         DOCTEST_DO_BINARY_EXPRESSION_COMPARISON(!=, " != ", DOCTEST_CMP_NE) //!OCLINT bitwise operator in conditional
          |                                                 ^

--- doctest/parts/doctest_fwd.h
+++ doctest/parts/doctest_fwd.h
@@ -1076,7 +1076,7 @@ DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wunused-comparison")
 // If not it doesn't find the operator or if the operator at global scope is defined after
 // this template, the template won't be instantiated due to SFINAE. Once the template is not
 // instantiated it can look for global operator using normal conversions.
-#define SFINAE_OP(ret,op) decltype(doctest::detail::declval<L>() op doctest::detail::declval<R>(),static_cast<ret>(0))
+#define SFINAE_OP(ret,op) decltype((void)(doctest::detail::declval<L>() op doctest::detail::declval<R>()),static_cast<ret>(0))
 
 #define DOCTEST_DO_BINARY_EXPRESSION_COMPARISON(op, op_str, op_macro)                              \
     template <typename R>                                                                          \
