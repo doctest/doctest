7339b37eb83c39e6c2304ea8f534bf235eaa4b1e
Josh Soref <jsoref@users.noreply.github.com>
2021-11-05 03:22:57 -0400

    spelling: for
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- doctest/doctest.h
+++ doctest/doctest.h
@@ -1220,7 +1220,7 @@ DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wunused-comparison")
                 , m_at(at) {}
 
         DOCTEST_NOINLINE operator Result() {
-// this is needed only foc MSVC 2015:
+// this is needed only for MSVC 2015:
 // https://ci.appveyor.com/project/onqtam/doctest/builds/38181202
 DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4800) // 'int': forcing value to bool
             bool res = static_cast<bool>(lhs);
