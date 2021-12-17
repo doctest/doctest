29d7599d1c28b75069d2bf7d50b75b5ce9dcb4af
Ilya Isaev <ilya.isaev@intel.com>
2021-12-17 18:13:26 +0300

    Change check
    Signed-off-by: Ilya Isaev <ilya.isaev@intel.com>

--- doctest/doctest.h
+++ doctest/doctest.h
@@ -255,7 +255,7 @@ DOCTEST_MSVC_SUPPRESS_WARNING(4623) // default constructor was implicitly define
 // MSVC++ 8.0       _MSC_VER == 1400 (Visual Studio 2005)
 
 // Universal Windows Platform support
-#ifdef WINAPI_FAMILY
+#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
 #define DOCTEST_CONFIG_NO_WINDOWS_SEH
 #endif // WINAPI_FAMILY
 #if DOCTEST_MSVC && !defined(DOCTEST_CONFIG_WINDOWS_SEH)
