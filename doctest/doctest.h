a275b8be58252d400d8c3a6d2686254bc0e10700
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-01-05 19:53:03 +0100

    Is this it??

--- doctest/doctest.h
+++ doctest/doctest.h
@@ -3120,8 +3120,7 @@ namespace detail {
             std::streampos pos = stack.back();
             stack.pop_back();
             unsigned sz = static_cast<unsigned>(ss.tellp() - pos);
-            ss.seekg(pos);
-            ss.seekp(pos);
+            ss.rdbuf()->pubseekpos(pos, std::ios::in | std::ios::out);
             return String(ss, sz);
         }
     } g_oss;
