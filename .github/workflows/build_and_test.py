8f16ba709139332ccfd8d6759b44facbe6590a96
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-01-08 22:40:37 +0100

    Retrace steps

--- .github/workflows/build_and_test.py
+++ .github/workflows/build_and_test.py
@@ -38,8 +38,7 @@ def run_test(build_type, test_mode, flags, test = True):
         f"cmake -S . "
         f"-B build "
         f"-D CMAKE_BUILD_TYPE={build_type} "
-        f'-D CXX={used_cxx} '
-        f'-D CC={used_cxx}'
+        f'-D CMAKE_CXX_COMPILER={used_cxx}'
     ):
         exit(2)
     if log_and_call("cmake --build build"):
