adf21cc8e505899ba985b1a82de99f749667f6a6
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-05-05 17:41:33 +0200

    Move test around

--- examples/all_features/assertion_macros.cpp
+++ examples/all_features/assertion_macros.cpp
@@ -1,5 +1,22 @@
+namespace A {
+    struct Bar {
+        int i;
+    };
+}
+
+bool operator==(const A::Bar& lhs, const A::Bar& rhs) {
+    return lhs.i == rhs.i;
+}
+
 #define DOCTEST_CONFIG_ASSERTS_RETURN_VALUES
 #include <doctest/doctest.h>
+
+TEST_CASE("equality of namespaced type") {
+    const auto b0 = A::Bar{};
+    const auto b1 = A::Bar{};
+    CHECK(b0 == b1);
+    CHECK_EQ(b0, b1);
+}
 
 #include "header.h"
 
@@ -232,20 +249,3 @@ TEST_CASE("check return values no print") {
     if (CHECK_THROWS_WITH(throw_if(true, 2), "1")) { MESSAGE(":D"); }
 }
 DOCTEST_MSVC_SUPPRESS_WARNING_POP
-
-namespace A {
-    struct Bar {
-        int i;
-    };
-}
-
-bool operator==(const A::Bar& lhs, const A::Bar& rhs) {
-    return lhs.i == rhs.i;
-}
-
-TEST_CASE("equality of namespaced type") {
-    const auto b0 = A::Bar{};
-    const auto b1 = A::Bar{};
-    CHECK(b0 == b1);
-    CHECK_EQ(b0, b1);
-}
