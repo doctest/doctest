bf4863d2e5a030c353a3afa079f803173de2a15b
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-06-08 03:54:10 +0300

    Cleanup of DOCTEST_DO_BINARY_EXPRESSION_COMPARISON. Fixes #651 (#652)
    * Cleanup of DOCTEST_DO_BINARY_EXPRESSION_COMPARISON. Fixes #651
    
    * Add test
    
    * Remove redundant assertion and default member initializer
    
    * Add extra test
    
    * Suppression
    
    Co-authored-by: Salvage <29021710+Saalvage@users.noreply.github.com>

--- examples/all_features/decomposition.cpp
+++ examples/all_features/decomposition.cpp
@@ -24,4 +24,40 @@ static MoveOnly genType(bool b) {
 TEST_CASE("Move Only Type") {
     CHECK(genType(true));
     CHECK(genType(false));
+
+    MoveOnly a{ 0 };
+    CHECK(a);
 }
+
+
+struct int_pointer {
+    int* p = nullptr;
+    int_pointer() = default;
+
+    // non-const ref used in constructor
+    // we don't want to accidentally construct a dangling pointer from a temporary
+    int_pointer(int& i) : p(&i) { }
+
+    // NOLINTNEXTLINE(readability-make-member-function-const)
+    explicit operator bool() { return !!p; }
+    int val() const { return *p; }
+};
+
+struct int_holder {
+    int i;
+    bool operator==(int_pointer p) const {
+        if (!p) return false;
+        return i == p.val();
+    }
+};
+
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4866) // eval order
+TEST_CASE("Impl cast from non-const value") {
+    int_holder h{ 8 };
+    int i = 8;
+
+    // this comparison is safe
+    // int_pointer can be implicitly constructed from this int here
+    CHECK(h == i);
+}
+DOCTEST_MSVC_SUPPRESS_WARNING_POP
