e557751b19c8d598e3bb4f2b3f4cc0b33eef2cd6
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-10-01 20:19:20 +0200

    Fix warnings

--- doctest/parts/doctest_fwd.h
+++ doctest/parts/doctest_fwd.h
@@ -1160,16 +1160,12 @@ DOCTEST_MSVC_SUPPRESS_WARNING_POP
 
     template <typename T>
     struct filldata<T*> {
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4180)
         static void fill(std::ostream* stream, const T* in) {
+DOCTEST_MSVC_SUPPRESS_WARNING_POP
+DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wmicrosoft-cast")
             filldata<const void*>::fill(stream, reinterpret_cast<const void*>(in));
-        }
-    };
-
-    template <typename T, typename... Args>
-    struct filldata<T(*)(Args...)> {
-        static void fill(std::ostream* stream, T(*in)(Args...)) {
-            if (in) { *stream << in; }
-            else { *stream << "nullptr"; }
+DOCTEST_CLANG_SUPPRESS_WARNING_POP
         }
     };
 }
