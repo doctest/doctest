0c04c9683af1c023a2f1bfd38b685e833bc8a58d
Sprite <SpriteOvO@gmail.com>
2022-10-02 23:03:43 +0800

    Fix compile errors when comparing pointers. (#699)
    * Explicitly convert pointer in `filldata<T*>::fill`
    
    * Add regression test
    
    * Increase standard compliance
    
    * Collect test output
    
    * Collect test output
    
    * Fix warnings
    
    * Add GCC workaround
    
    * Update doctest.h
    
    * Move __extension__
    
    * Use different workaround
    
    * Add missing const qualifier
    
    Co-authored-by: Salvage <29021710+Saalvage@users.noreply.github.com>

--- doctest/parts/doctest_fwd.h
+++ doctest/parts/doctest_fwd.h
@@ -1160,8 +1160,18 @@ DOCTEST_MSVC_SUPPRESS_WARNING_POP
 
     template <typename T>
     struct filldata<T*> {
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4180)
         static void fill(std::ostream* stream, const T* in) {
-            filldata<const void*>::fill(stream, in);
+DOCTEST_MSVC_SUPPRESS_WARNING_POP
+DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wmicrosoft-cast")
+            filldata<const void*>::fill(stream,
+#if DOCTEST_GCC == 0 || DOCTEST_GCC >= DOCTEST_COMPILER(4, 9, 0)
+                reinterpret_cast<const void*>(in)
+#else
+                *reinterpret_cast<const void* const*>(&in)
+#endif
+            );
+DOCTEST_CLANG_SUPPRESS_WARNING_POP
         }
     };
 }
