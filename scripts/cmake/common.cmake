7a96b3ae270cf4c7b062d95f652a016de7dde45e
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-10-06 19:58:27 +0200

    Update common.cmake

--- scripts/cmake/common.cmake
+++ scripts/cmake/common.cmake
@@ -81,6 +81,11 @@ endmacro()
 if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     add_compiler_flags(-Werror)
 
+    message(CMAKE_CXX_COMPILER_ID)
+    message(CMAKE_C_COMPILER_ID)
+    message(CMAKE_CXX_COMPILER)
+    message(CMAKE_C_COMPILER)
+
     # The following options are not valid when clang-cl is used.
     if(NOT MSVC)
         add_compiler_flags(-pedantic)
