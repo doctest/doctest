42b564c90d2b133c47e3a3dba401b8d71e5f9370
helmesjo <helmesjo@gmail.com>
2024-06-19 15:30:47 +0200

    Use same naming for file descriptors as catch3 (see here: https://github.com/catchorg/Catch2/blob/4e8d92bf02f7d1c8006a0e7a5ecabd8e62d98502/docs/command-line.md#sending-output-to-a-file).

--- doctest/parts/doctest.cpp
+++ doctest/parts/doctest.cpp
@@ -3039,8 +3039,8 @@ namespace {
             s << " -" DOCTEST_OPTIONS_PREFIX_DISPLAY "r,   --" DOCTEST_OPTIONS_PREFIX_DISPLAY "reporters=<filters>           "
               << Whitespace(sizePrefixDisplay*1) << "reporters to use (console is default)\n";
             s << " -" DOCTEST_OPTIONS_PREFIX_DISPLAY "o,   --" DOCTEST_OPTIONS_PREFIX_DISPLAY "out=<string>                  "
-              << Whitespace(sizePrefixDisplay*1) << "output destination (cout<< is default)\n";
-            s << Whitespace(sizePrefixDisplay*3) << "                                       <string> - [filepath/cout<</cerr<<]\n";
+              << Whitespace(sizePrefixDisplay*1) << "output destination (%stdout is default)\n";
+            s << Whitespace(sizePrefixDisplay*3) << "                                       <string> - [filepath/%stdout/%stderr]\n";
             s << " -" DOCTEST_OPTIONS_PREFIX_DISPLAY "ob,  --" DOCTEST_OPTIONS_PREFIX_DISPLAY "order-by=<string>             "
               << Whitespace(sizePrefixDisplay*1) << "how the tests should be ordered\n";
             s << Whitespace(sizePrefixDisplay*3) << "                                       <string> - [file/suite/name/rand/none]\n";
@@ -3569,7 +3569,7 @@ void Context::parseArgs(int argc, const char* const* argv, bool withDefaults) {
     p->var = strRes
 
     // clang-format off
-    DOCTEST_PARSE_STR_OPTION("out", "o", out, "cout<<");
+    DOCTEST_PARSE_STR_OPTION("out", "o", out, "%stdout");
     DOCTEST_PARSE_STR_OPTION("order-by", "ob", order_by, "file");
     DOCTEST_PARSE_INT_OPTION("rand-seed", "rs", rand_seed, 0);
 
@@ -3718,13 +3718,13 @@ int Context::run() {
     if(p->cout == nullptr) {
         if(p->quiet) {
             p->cout = &discardOut;
-        } else if(p->out == "cout<<") {
+        } else if(p->out == "%stdout") {
 #ifndef DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
             p->cout = &std::cout;
 #else
             return EXIT_FAILURE;
 #endif // DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
-        } else if(p->out == "cerr<<") {
+        } else if(p->out == "%stderr") {
 #ifndef DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
             p->cout = &std::cerr;
 #else
