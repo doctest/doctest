bcfad42c781541bd54fac5597f5eb43ffd929373
Max Ferger <max.ferger@gmx.de>
2024-11-26 15:56:15 +0100

    feat: option to strip a filename prefixes from the output

--- doctest/parts/doctest_fwd.h
+++ doctest/parts/doctest_fwd.h
@@ -922,6 +922,7 @@ struct ContextOptions //!OCLINT too many fields
     bool no_skip;              // don't skip test cases which are marked to be skipped
     bool gnu_file_line;        // if line numbers should be surrounded with :x: and not (x):
     bool no_path_in_filenames; // if the path to files should be removed from the output
+    String strip_file_prefixes;// remove the longest matching one of these prefixes from any file paths in the output
     bool no_line_numbers;      // if source code line numbers should be omitted from the output
     bool no_debug_output;      // no output in the debug console when a debugger is attached
     bool no_skipped_summary;   // don't print "skipped" in the summary !!! UNDOCUMENTED !!!
--- scripts/cmake/common.cmake
+++ scripts/cmake/common.cmake
@@ -38,8 +38,8 @@ function(doctest_add_test_impl)
         set(ARG_NAME ${ARG_NAME}_junit)
     endif()
 
-    # append the argument for removing paths from filenames in the output so tests give the same output everywhere
-    set(the_command "${the_command} --dt-no-path-filenames=1")
+    # append the argument for stripping prefixes from filenames in the output so tests give the same output everywhere
+    set(the_command "${the_command} --dt-strip-file-prefixes=${CMAKE_SOURCE_DIR}/:${CMAKE_BINARY_DIR}/")
     # append the argument for substituting source line numbers with 0 in the output so tests give the same output when lines change a bit
     set(the_command "${the_command} --dt-no-line-numbers=1")
     # append the argument for ignoring the exit code of the test programs because some are intended to have failing tests
