822abbc6aaeece483a92a71c9019c769441be39e
Seth Dale <89610426+sddale@users.noreply.github.com>
2025-02-13 13:27:49 -0700

    Python 3 update to range_based_execution.py

--- examples/range_based_execution.py
+++ examples/range_based_execution.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import math
@@ -9,35 +9,49 @@ if len(sys.argv) < 2:
     print("supply the path to the doctest executable as the first argument!")
     sys.exit(1)
 
-# get the number of tests in the doctest executable
+# Get the number of tests in the doctest executable
 num_tests = 0
 
-program_with_args = [sys.argv[1], "--dt-count=1"]
+program_with_args = [sys.argv[1], "--dt-count"]
 for i in range(2, len(sys.argv)):
     program_with_args.append(sys.argv[i])
 
-result = subprocess.Popen(program_with_args, stdout = subprocess.PIPE).communicate()[0]
+result = (
+    subprocess.Popen(program_with_args, stdout=subprocess.PIPE)
+    .communicate()[0]
+    .decode("utf-8")
+)
 result = result.splitlines(True)
+
 for line in result:
-    if line.startswith("[doctest] unskipped test cases passing the current filters:"):
-        num_tests = int(line.rsplit(' ', 1)[-1])
+    if line.startswith(
+        "[doctest] unskipped test cases passing the current filters:"
+    ):
+        num_tests = int(line.rsplit(" ", 1)[-1])
 
-# calculate the ranges
+# Calculate the ranges
 cores = multiprocessing.cpu_count()
-l = range(num_tests + 1)
-n = int(math.ceil(float(len( l )) / cores))
-data = [l[i : i + n] for i in range(1, len( l ), n)]
-data = tuple([[x[0], x[-1]] for x in data])
+l = list(range(num_tests + 1))
+n = int(math.ceil(len(l) / cores))
+data = [l[i : i + n] for i in range(1, len(l), n)]
+data = [(x[0], x[-1]) for x in data]
 
-# for 8 cores and 100 tests the ranges will look like this
-# ([1, 13], [14, 26], [27, 39], [40, 52], [53, 65], [66, 78], [79, 91], [92, 100])
 
-# the worker callback that runs the executable for the given range of tests
-def worker((first, last)):
-    program_with_args = [sys.argv[1], "--dt-first=" + str(first), "--dt-last=" + str(last)]
+# Worker function that runs the executable for the given range of tests
+def worker(first_last):
+    first, last = first_last
+    program_with_args = [
+        sys.argv[1],
+        f"--dt-first={first}",
+        f"--dt-last={last}",
+    ]
     subprocess.Popen(program_with_args)
 
-# run the tasks on a pool
-if __name__ == '__main__':
+
+# Run the tasks on a pool
+if __name__ == "__main__":
+    multiprocessing.set_start_method(
+        "spawn", force=True
+    )  # Ensures compatibility on Windows
     p = multiprocessing.Pool(cores)
     p.map(worker, data)
