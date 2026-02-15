1f3aaa52c2d13ed34039216ff3b4b6bea5d7fca1
Mitchell Grout <mitchell.grout@hotmail.com>
2026-02-15 04:22:08 +0000

    Add dedicated regression tests

---
+++ tests/regression/test.630.cpp
@@ -0,0 +1,37 @@
+#include <doctest/doctest.h>
+
+namespace {
+
+DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wunused-member-function")
+class MoveOnly {
+public:
+    MoveOnly(int iIn) : i(iIn) { }
+    MoveOnly(MoveOnly&&) = default;
+    MoveOnly(const MoveOnly&) = delete;
+    MoveOnly& operator=(MoveOnly&&) = default;
+    MoveOnly& operator=(const MoveOnly&) = default;
+    ~MoveOnly() = default;
+    // NOLINTNEXTLINE(readability-make-member-function-const)
+    operator bool() const { // both non-const and const can call
+        return i == 42;
+    }
+
+private:
+    int i;
+};
+DOCTEST_CLANG_SUPPRESS_WARNING_POP
+
+MoveOnly genType(bool b) {
+    return { b ? 42 : 0 };
+}
+
+} // namespace
+
+
+TEST_CASE("Move Only Type") {
+    CHECK(genType(true));
+    CHECK_FALSE(genType(false));
+
+    MoveOnly a{ 0 };
+    CHECK_FALSE(a);
+}
---
+++ tests/regression/test.651.cpp
@@ -0,0 +1,40 @@
+#include <doctest/doctest.h>
+
+namespace {
+
+DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wunused-member-function")
+struct int_pointer {
+    int* p = nullptr;
+    int_pointer() = default;
+
+    // non-const ref used in constructor
+    // we don't want to accidentally construct a dangling pointer from a temporary
+    int_pointer(int& i) : p(&i) { }
+
+    // NOLINTNEXTLINE(readability-make-member-function-const)
+    explicit operator bool() { return !!p; }
+    int val() const { return *p; }
+};
+DOCTEST_CLANG_SUPPRESS_WARNING_POP
+
+struct int_holder {
+    int i;
+    bool operator==(int_pointer p) const {
+        if (!p) return false;
+        return i == p.val();
+    }
+};
+
+} // namespace
+
+
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4866) // eval order
+TEST_CASE("Impl cast from non-const value") {
+    int_holder h{ 8 };
+    int i = 8;
+
+    // this comparison is safe
+    // int_pointer can be implicitly constructed from this int here
+    CHECK(h == i);
+}
+DOCTEST_MSVC_SUPPRESS_WARNING_POP
