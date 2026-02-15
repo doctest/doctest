6e098dbba719cb9a54db633c89f4fe7e7d651b07
Mitchell Grout <mitchgrout@users.noreply.github.com>
2026-02-15 22:12:08 +1300

    chore: Add project regression tests (#1037)
    * Add dedicated regression tests
    
    * Drop existing regression tests from example suite

--- tests/regression/test.651.cpp
+++ tests/regression/test.651.cpp
@@ -1,35 +1,8 @@
 #include <doctest/doctest.h>
 
-class MoveOnly {
-    public:
-        MoveOnly(int iIn) : i(iIn) { }
-        MoveOnly(MoveOnly&&) = default;
-        MoveOnly(const MoveOnly&) = delete;
-        MoveOnly& operator=(MoveOnly&&) = default;
-        MoveOnly& operator=(const MoveOnly&) = default;
-        ~MoveOnly() = default;
-        // NOLINTNEXTLINE(readability-make-member-function-const)
-        operator bool() const { // both non-const and const can call
-            return i == 42;
-        }
+namespace {
 
-    private:
-        int i;
-};
-
-static MoveOnly genType(bool b) {
-    return { b ? 42 : 0 };
-}
-
-TEST_CASE("Move Only Type") {
-    CHECK(genType(true));
-    CHECK(genType(false));
-
-    MoveOnly a{ 0 };
-    CHECK(a);
-}
-
-
+DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wunused-member-function")
 struct int_pointer {
     int* p = nullptr;
     int_pointer() = default;
@@ -42,6 +15,7 @@ struct int_pointer {
     explicit operator bool() { return !!p; }
     int val() const { return *p; }
 };
+DOCTEST_CLANG_SUPPRESS_WARNING_POP
 
 struct int_holder {
     int i;
@@ -50,6 +24,9 @@ struct int_holder {
         return i == p.val();
     }
 };
+
+} // namespace
+
 
 DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4866) // eval order
 TEST_CASE("Impl cast from non-const value") {
---
+++ tests/regression/test.786.cpp
@@ -0,0 +1,36 @@
+#include <doctest/doctest.h>
+#include <cstdint>
+
+
+TEST_CASE("Bitfield") {
+    struct S {
+        uint32_t a: 31;
+        uint32_t b: 1;
+    };
+
+    S s;
+    s.a = 0;
+    s.b = 1;
+    CHECK(s.a == 0);
+}
+
+
+TEST_CASE("Packed struct") {
+    DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wpacked")
+#if !defined(WIN32)
+    struct __attribute__((packed)) P {
+        int i;
+    };
+#else
+    #pragma pack(push, 1)
+    struct P {
+        int i;
+    };
+    #pragma pack(pop)
+#endif
+    DOCTEST_CLANG_SUPPRESS_WARNING_POP
+
+    P p;
+    p.i = 0;
+    CHECK(p.i == 0);
+}
