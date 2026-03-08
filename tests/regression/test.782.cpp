f1c5e9711aa9d44329d337eaec551a574c8f7c16
Arthur Ozga <aozgaa@gmail.com>
2026-03-07 22:29:52 -0500

    782 repro -- exceptions disabled

--- tests/regression/test.782.cpp
+++ tests/regression/test.782.cpp
@@ -2,6 +2,7 @@
 #include <doctest/doctest.h>
 
 TEST_CASE("inconsistent init before subcases") {
+#ifndef DOCTEST_CONFIG_NO_EXCEPTIONS
     static int cnt = 0;
     if (++cnt == 2) {
         throw std::runtime_error{"#782 repro exception"};
@@ -9,4 +10,7 @@ TEST_CASE("inconsistent init before subcases") {
     DOCTEST_SUBCASE("1") {}
     DOCTEST_SUBCASE("2") {}
     DOCTEST_SUBCASE("3") {}
+#else
+    FAIL("Exceptions disabled, cannot run this test");
+#endif // DOCTEST_CONFIG_NO_EXCEPTIONS
 }
