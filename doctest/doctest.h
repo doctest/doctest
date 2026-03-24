32d4813902f79e8b2a0c1204f651d74c7849cdae
Mitchell Grout <mitchgrout@users.noreply.github.com>
2026-03-24 21:44:09 +1300

    fix: Ensure all reporters are registered when using doctest_with_main (#1072)

--- doctest/doctest.h
+++ doctest/doctest.h
@@ -5112,6 +5112,298 @@ DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
 
 #endif // DOCTEST_PARTS_PRIVATE_SIGNALS
 
+// Fix for #1035
+#ifndef DOCTEST_PARTS_PRIVATE_REPORTERS_JUNIT
+#define DOCTEST_PARTS_PRIVATE_REPORTERS_JUNIT
+
+#ifndef DOCTEST_PARTS_PRIVATE_XML
+#define DOCTEST_PARTS_PRIVATE_XML
+
+
+DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
+
+#ifndef DOCTEST_CONFIG_DISABLE
+
+namespace doctest {
+namespace detail {
+
+// =================================================================================================
+// The following code has been taken verbatim from Catch2/include/internal/catch_xmlwriter.h
+// This is done so cherry-picking bug fixes is trivial - even the style/formatting is untouched.
+// =================================================================================================
+/* clang-format off */ /* NOLINTBEGIN */
+
+    class XmlEncode {
+    public:
+        enum ForWhat { ForTextNodes, ForAttributes };
+
+        XmlEncode( std::string const& str, ForWhat forWhat = ForTextNodes );
+
+        void encodeTo( std::ostream& os ) const;
+
+        friend std::ostream& operator << ( std::ostream& os, XmlEncode const& xmlEncode );
+
+    private:
+        std::string m_str;
+        ForWhat m_forWhat;
+    };
+
+    class XmlWriter {
+    public:
+
+        class ScopedElement {
+        public:
+            ScopedElement( XmlWriter* writer );
+
+            ScopedElement( ScopedElement&& other ) DOCTEST_NOEXCEPT;
+            ScopedElement& operator=( ScopedElement&& other ) DOCTEST_NOEXCEPT;
+
+            ~ScopedElement();
+
+            ScopedElement& writeText( std::string const& text, bool indent = true );
+
+            template<typename T>
+            ScopedElement& writeAttribute( std::string const& name, T const& attribute ) {
+                m_writer->writeAttribute( name, attribute );
+                return *this;
+            }
+
+        private:
+            mutable XmlWriter* m_writer = nullptr;
+        };
+
+#ifndef DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
+        XmlWriter( std::ostream& os = std::cout );
+#else // DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
+        XmlWriter( std::ostream& os );
+#endif // DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
+        ~XmlWriter();
+
+        XmlWriter( XmlWriter const& ) = delete;
+        XmlWriter& operator=( XmlWriter const& ) = delete;
+
+        XmlWriter& startElement( std::string const& name );
+
+        ScopedElement scopedElement( std::string const& name );
+
+        XmlWriter& endElement();
+
+        XmlWriter& writeAttribute( std::string const& name, std::string const& attribute );
+
+        XmlWriter& writeAttribute( std::string const& name, const char* attribute );
+
+        XmlWriter& writeAttribute( std::string const& name, bool attribute );
+
+        template<typename T>
+        XmlWriter& writeAttribute( std::string const& name, T const& attribute ) {
+        std::stringstream rss;
+            rss << attribute;
+            return writeAttribute( name, rss.str() );
+        }
+
+        XmlWriter& writeText( std::string const& text, bool indent = true );
+
+        //XmlWriter& writeComment( std::string const& text );
+
+        //void writeStylesheetRef( std::string const& url );
+
+        //XmlWriter& writeBlankLine();
+
+        void ensureTagClosed();
+
+        void writeDeclaration();
+
+    private:
+
+        void newlineIfNecessary();
+
+        bool m_tagIsOpen = false;
+        bool m_needsNewline = false;
+        std::vector<std::string> m_tags;
+        std::string m_indent;
+        std::ostream& m_os;
+    };
+
+/* clang-format on */ /* NOLINTEND */
+// =================================================================================================
+// End of copy-pasted code from Catch
+// =================================================================================================
+
+} // namespace detail
+} // namespace doctest
+
+#endif // DOCTEST_CONFIG_DISABLE
+
+DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
+
+#endif // DOCTEST_PARTS_PRIVATE_XML
+
+DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
+
+#ifndef DOCTEST_CONFIG_DISABLE
+
+namespace doctest {
+
+// TODO:
+// - log_message()
+// - respond to queries
+// - honor remaining options
+// - more attributes in tags
+struct JUnitReporter : public IReporter {
+    detail::XmlWriter xml;
+    DOCTEST_DECLARE_MUTEX(mutex)
+    detail::Timer timer;
+    std::vector<String> deepestSubcaseStackNames;
+
+    struct JUnitTestCaseData {
+        static std::string getCurrentTimestamp();
+
+        struct JUnitTestMessage {
+            JUnitTestMessage(const std::string &_message, const std::string &_type, const std::string &_details);
+
+            JUnitTestMessage(const std::string &_message, const std::string &_details);
+
+            std::string message, type, details;
+        };
+
+        struct JUnitTestCase {
+            JUnitTestCase(const std::string &_classname, const std::string &_name);
+
+            std::string classname, name;
+            double time;
+            std::vector<JUnitTestMessage> failures, errors;
+        };
+
+        void add(const std::string &classname, const std::string &name);
+
+        void appendSubcaseNamesToLastTestcase(std::vector<String> nameStack);
+
+        void addTime(double time);
+
+        void addFailure(const std::string &message, const std::string &type, const std::string &details);
+
+        void addError(const std::string &message, const std::string &details);
+
+        std::vector<JUnitTestCase> testcases;
+        double totalSeconds = 0;
+        int totalErrors = 0, totalFailures = 0;
+    };
+
+    JUnitTestCaseData testCaseData;
+
+    // caching pointers/references to objects of these types - safe to do
+    const ContextOptions &opt;
+    const TestCaseData *tc = nullptr;
+
+    JUnitReporter(const ContextOptions &co);
+
+    unsigned line(unsigned l) const;
+
+    // =========================================================================================
+    // WHAT FOLLOWS ARE OVERRIDES OF THE VIRTUAL METHODS OF THE REPORTER INTERFACE
+    // =========================================================================================
+
+    void report_query(const QueryData &) override;
+
+    void test_run_start() override;
+
+    void test_run_end(const TestRunStats &p) override;
+
+    void test_case_start(const TestCaseData &in) override;
+
+    void test_case_reenter(const TestCaseData &in) override;
+
+    void test_case_end(const CurrentTestCaseStats &) override;
+
+    void test_case_exception(const TestCaseException &e) override;
+
+    void subcase_start(const SubcaseSignature &in) override;
+
+    void subcase_end() override;
+
+    void log_assert(const AssertData &rb) override;
+
+    void log_message(const MessageData &mb) override;
+
+    void test_case_skipped(const TestCaseData &) override;
+
+    static void log_contexts(std::ostringstream &s);
+};
+
+DOCTEST_REGISTER_REPORTER("junit", 0, JUnitReporter);
+
+} // namespace doctest
+
+#endif // DOCTEST_CONFIG_DISABLE
+
+DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
+
+#endif // DOCTEST_PARTS_PRIVATE_REPORTERS_JUNIT
+#ifndef DOCTEST_PARTS_PRIVATE_REPORTERS_XML
+#define DOCTEST_PARTS_PRIVATE_REPORTERS_XML
+
+
+DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
+
+#ifndef DOCTEST_CONFIG_DISABLE
+
+namespace doctest {
+
+struct XmlReporter : public IReporter {
+    detail::XmlWriter xml;
+    DOCTEST_DECLARE_MUTEX(mutex)
+
+    // caching pointers/references to objects of these types - safe to do
+    const ContextOptions &opt;
+    const TestCaseData *tc = nullptr;
+
+    XmlReporter(const ContextOptions &co);
+
+    void log_contexts();
+
+    unsigned line(unsigned l) const;
+
+    void test_case_start_impl(const TestCaseData &in);
+
+    // =========================================================================================
+    // WHAT FOLLOWS ARE OVERRIDES OF THE VIRTUAL METHODS OF THE REPORTER INTERFACE
+    // =========================================================================================
+
+    void report_query(const QueryData &in) override;
+
+    void test_run_start() override;
+
+    void test_run_end(const TestRunStats &p) override;
+
+    void test_case_start(const TestCaseData &in) override;
+
+    void test_case_reenter(const TestCaseData &) override;
+
+    void test_case_end(const CurrentTestCaseStats &st) override;
+
+    void test_case_exception(const TestCaseException &e) override;
+
+    void subcase_start(const SubcaseSignature &in) override;
+
+    void subcase_end() override;
+
+    void log_assert(const AssertData &rb) override;
+
+    void log_message(const MessageData &mb) override;
+
+    void test_case_skipped(const TestCaseData &in) override;
+};
+
+DOCTEST_REGISTER_REPORTER("xml", 0, XmlReporter);
+
+} // namespace doctest
+
+#endif // DOCTEST_CONFIG_DISABLE
+
+DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
+
+#endif // DOCTEST_PARTS_PRIVATE_REPORTERS_XML
+
 DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
 
 namespace doctest {
@@ -7010,232 +7302,6 @@ DOCTEST_DEBUG_OUTPUT_REPORTER_OVERRIDE(test_case_skipped, const TestCaseData &, 
 #endif // DOCTEST_CONFIG_DISABLE
 
 DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
-#ifndef DOCTEST_PARTS_PRIVATE_REPORTERS_JUNIT
-#define DOCTEST_PARTS_PRIVATE_REPORTERS_JUNIT
-
-#ifndef DOCTEST_PARTS_PRIVATE_XML
-#define DOCTEST_PARTS_PRIVATE_XML
-
-
-DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
-
-#ifndef DOCTEST_CONFIG_DISABLE
-
-namespace doctest {
-namespace detail {
-
-// =================================================================================================
-// The following code has been taken verbatim from Catch2/include/internal/catch_xmlwriter.h
-// This is done so cherry-picking bug fixes is trivial - even the style/formatting is untouched.
-// =================================================================================================
-/* clang-format off */ /* NOLINTBEGIN */
-
-    class XmlEncode {
-    public:
-        enum ForWhat { ForTextNodes, ForAttributes };
-
-        XmlEncode( std::string const& str, ForWhat forWhat = ForTextNodes );
-
-        void encodeTo( std::ostream& os ) const;
-
-        friend std::ostream& operator << ( std::ostream& os, XmlEncode const& xmlEncode );
-
-    private:
-        std::string m_str;
-        ForWhat m_forWhat;
-    };
-
-    class XmlWriter {
-    public:
-
-        class ScopedElement {
-        public:
-            ScopedElement( XmlWriter* writer );
-
-            ScopedElement( ScopedElement&& other ) DOCTEST_NOEXCEPT;
-            ScopedElement& operator=( ScopedElement&& other ) DOCTEST_NOEXCEPT;
-
-            ~ScopedElement();
-
-            ScopedElement& writeText( std::string const& text, bool indent = true );
-
-            template<typename T>
-            ScopedElement& writeAttribute( std::string const& name, T const& attribute ) {
-                m_writer->writeAttribute( name, attribute );
-                return *this;
-            }
-
-        private:
-            mutable XmlWriter* m_writer = nullptr;
-        };
-
-#ifndef DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
-        XmlWriter( std::ostream& os = std::cout );
-#else // DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
-        XmlWriter( std::ostream& os );
-#endif // DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
-        ~XmlWriter();
-
-        XmlWriter( XmlWriter const& ) = delete;
-        XmlWriter& operator=( XmlWriter const& ) = delete;
-
-        XmlWriter& startElement( std::string const& name );
-
-        ScopedElement scopedElement( std::string const& name );
-
-        XmlWriter& endElement();
-
-        XmlWriter& writeAttribute( std::string const& name, std::string const& attribute );
-
-        XmlWriter& writeAttribute( std::string const& name, const char* attribute );
-
-        XmlWriter& writeAttribute( std::string const& name, bool attribute );
-
-        template<typename T>
-        XmlWriter& writeAttribute( std::string const& name, T const& attribute ) {
-        std::stringstream rss;
-            rss << attribute;
-            return writeAttribute( name, rss.str() );
-        }
-
-        XmlWriter& writeText( std::string const& text, bool indent = true );
-
-        //XmlWriter& writeComment( std::string const& text );
-
-        //void writeStylesheetRef( std::string const& url );
-
-        //XmlWriter& writeBlankLine();
-
-        void ensureTagClosed();
-
-        void writeDeclaration();
-
-    private:
-
-        void newlineIfNecessary();
-
-        bool m_tagIsOpen = false;
-        bool m_needsNewline = false;
-        std::vector<std::string> m_tags;
-        std::string m_indent;
-        std::ostream& m_os;
-    };
-
-/* clang-format on */ /* NOLINTEND */
-// =================================================================================================
-// End of copy-pasted code from Catch
-// =================================================================================================
-
-} // namespace detail
-} // namespace doctest
-
-#endif // DOCTEST_CONFIG_DISABLE
-
-DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
-
-#endif // DOCTEST_PARTS_PRIVATE_XML
-
-DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
-
-#ifndef DOCTEST_CONFIG_DISABLE
-
-namespace doctest {
-
-// TODO:
-// - log_message()
-// - respond to queries
-// - honor remaining options
-// - more attributes in tags
-struct JUnitReporter : public IReporter {
-    detail::XmlWriter xml;
-    DOCTEST_DECLARE_MUTEX(mutex)
-    detail::Timer timer;
-    std::vector<String> deepestSubcaseStackNames;
-
-    struct JUnitTestCaseData {
-        static std::string getCurrentTimestamp();
-
-        struct JUnitTestMessage {
-            JUnitTestMessage(const std::string &_message, const std::string &_type, const std::string &_details);
-
-            JUnitTestMessage(const std::string &_message, const std::string &_details);
-
-            std::string message, type, details;
-        };
-
-        struct JUnitTestCase {
-            JUnitTestCase(const std::string &_classname, const std::string &_name);
-
-            std::string classname, name;
-            double time;
-            std::vector<JUnitTestMessage> failures, errors;
-        };
-
-        void add(const std::string &classname, const std::string &name);
-
-        void appendSubcaseNamesToLastTestcase(std::vector<String> nameStack);
-
-        void addTime(double time);
-
-        void addFailure(const std::string &message, const std::string &type, const std::string &details);
-
-        void addError(const std::string &message, const std::string &details);
-
-        std::vector<JUnitTestCase> testcases;
-        double totalSeconds = 0;
-        int totalErrors = 0, totalFailures = 0;
-    };
-
-    JUnitTestCaseData testCaseData;
-
-    // caching pointers/references to objects of these types - safe to do
-    const ContextOptions &opt;
-    const TestCaseData *tc = nullptr;
-
-    JUnitReporter(const ContextOptions &co);
-
-    unsigned line(unsigned l) const;
-
-    // =========================================================================================
-    // WHAT FOLLOWS ARE OVERRIDES OF THE VIRTUAL METHODS OF THE REPORTER INTERFACE
-    // =========================================================================================
-
-    void report_query(const QueryData &) override;
-
-    void test_run_start() override;
-
-    void test_run_end(const TestRunStats &p) override;
-
-    void test_case_start(const TestCaseData &in) override;
-
-    void test_case_reenter(const TestCaseData &in) override;
-
-    void test_case_end(const CurrentTestCaseStats &) override;
-
-    void test_case_exception(const TestCaseException &e) override;
-
-    void subcase_start(const SubcaseSignature &in) override;
-
-    void subcase_end() override;
-
-    void log_assert(const AssertData &rb) override;
-
-    void log_message(const MessageData &mb) override;
-
-    void test_case_skipped(const TestCaseData &) override;
-
-    static void log_contexts(std::ostringstream &s);
-};
-
-DOCTEST_REGISTER_REPORTER("junit", 0, JUnitReporter);
-
-} // namespace doctest
-
-#endif // DOCTEST_CONFIG_DISABLE
-
-DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
-
-#endif // DOCTEST_PARTS_PRIVATE_REPORTERS_JUNIT
 
 DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
 
@@ -7459,70 +7525,6 @@ void JUnitReporter::log_contexts(std::ostringstream &s) {
 #endif // DOCTEST_CONFIG_DISABLE
 
 DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
-#ifndef DOCTEST_PARTS_PRIVATE_REPORTERS_XML
-#define DOCTEST_PARTS_PRIVATE_REPORTERS_XML
-
-
-DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
-
-#ifndef DOCTEST_CONFIG_DISABLE
-
-namespace doctest {
-
-struct XmlReporter : public IReporter {
-    detail::XmlWriter xml;
-    DOCTEST_DECLARE_MUTEX(mutex)
-
-    // caching pointers/references to objects of these types - safe to do
-    const ContextOptions &opt;
-    const TestCaseData *tc = nullptr;
-
-    XmlReporter(const ContextOptions &co);
-
-    void log_contexts();
-
-    unsigned line(unsigned l) const;
-
-    void test_case_start_impl(const TestCaseData &in);
-
-    // =========================================================================================
-    // WHAT FOLLOWS ARE OVERRIDES OF THE VIRTUAL METHODS OF THE REPORTER INTERFACE
-    // =========================================================================================
-
-    void report_query(const QueryData &in) override;
-
-    void test_run_start() override;
-
-    void test_run_end(const TestRunStats &p) override;
-
-    void test_case_start(const TestCaseData &in) override;
-
-    void test_case_reenter(const TestCaseData &) override;
-
-    void test_case_end(const CurrentTestCaseStats &st) override;
-
-    void test_case_exception(const TestCaseException &e) override;
-
-    void subcase_start(const SubcaseSignature &in) override;
-
-    void subcase_end() override;
-
-    void log_assert(const AssertData &rb) override;
-
-    void log_message(const MessageData &mb) override;
-
-    void test_case_skipped(const TestCaseData &in) override;
-};
-
-DOCTEST_REGISTER_REPORTER("xml", 0, XmlReporter);
-
-} // namespace doctest
-
-#endif // DOCTEST_CONFIG_DISABLE
-
-DOCTEST_SUPPRESS_PRIVATE_WARNINGS_POP
-
-#endif // DOCTEST_PARTS_PRIVATE_REPORTERS_XML
 
 DOCTEST_SUPPRESS_PRIVATE_WARNINGS_PUSH
 
