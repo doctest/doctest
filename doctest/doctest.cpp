da544d50a069a0b1232608162bcc1f27e91ea2f5
Mitchell Grout <mitchgrout@users.noreply.github.com>
2026-04-14 19:19:29 +0900

    build(cmake): Use single-source `doctest.cpp` for `doctest_with_main` target (#1106)

---
+++ doctest/doctest.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2016-2026 doctest.h Authors
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+// The documentation can be found at the library's page:
+// https://github.com/doctest/doctest/blob/master/doc/markdown/readme.md
+
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/doctest.h"
