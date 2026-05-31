0541fabafff9968470a4976d39d8deda51429647
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2026-05-31 12:01:44 +0100

    Suppress the "Specializing standard library template std::tuple is forbidden" warning

--- doctest/parts/public/std/fwd.h
+++ doctest/parts/public/std/fwd.h
@@ -33,8 +33,12 @@ basic_ostream<char, traits> &operator<<(basic_ostream<char, traits> &, const cha
 template <class charT, class traits>
 class basic_istream;
 typedef basic_istream<char, char_traits<char>> istream; // NOLINT(modernize-use-using)
-template <class... Types>
-class tuple;
+#ifdef _MSC_VER
+#pragma warning(suppress: 5285)        // Suppress "Specializing template std::tuple is forbidden" warning
+template <class... Types> class tuple; // on this line only
+#else
+template <class... Types> class tuple;
+#endif
 #if DOCTEST_MSVC >= DOCTEST_COMPILER(19, 20, 0)
 // see this issue on why this is needed: https://github.com/doctest/doctest/issues/183
 template <class Ty>
