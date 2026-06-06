c1edf630dc2f9871f1106cf04585181c210c38ad
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2026-06-06 09:44:46 +0100

    Reworked original structure but via macros

--- doctest/doctest.h
+++ doctest/doctest.h
@@ -684,7 +684,10 @@ template <class charT, class traits>
 class basic_istream;
 typedef basic_istream<char, char_traits<char>> istream; // NOLINT(modernize-use-using)
 template <class... Types>
+// Specializing template std::tuple is forbidden
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(5285)
 class tuple;
+DOCTEST_MSVC_SUPPRESS_WARNING_POP
 #if DOCTEST_MSVC >= DOCTEST_COMPILER(19, 20, 0)
 // see this issue on why this is needed: https://github.com/doctest/doctest/issues/183
 template <class Ty>
