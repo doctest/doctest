cec8f147830dce318a057b8c8f6d15b29695ff98
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2026-06-06 09:50:26 +0100

    Clang format changes

--- doctest/parts/public/std/fwd.h
+++ doctest/parts/public/std/fwd.h
@@ -34,8 +34,7 @@ template <class charT, class traits>
 class basic_istream;
 typedef basic_istream<char, char_traits<char>> istream; // NOLINT(modernize-use-using)
 template <class... Types>
-// Specializing template std::tuple is forbidden
-DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(5285)
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(5285) // Specializing template std::tuple is forbidden
 class tuple;
 DOCTEST_MSVC_SUPPRESS_WARNING_POP
 #if DOCTEST_MSVC >= DOCTEST_COMPILER(19, 20, 0)
