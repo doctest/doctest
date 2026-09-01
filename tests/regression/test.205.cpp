01dddd310a35e3305586549c9ea4890da2733598
otrempe <oliviertrempe@gmail.com>
2026-09-01 12:45:26 -0400

    Added a test to validate there is no apparent memory leaks
    The test exercises static registration, INFO() storage, stringification TLS storage, quiet output, and the debug-output reporter

---
+++ tests/regression/test.205.cpp
@@ -0,0 +1,38 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest/doctest.h>
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+#include <crtdbg.h>
+#include <doctest/parts/private/reporters/debug_output_window.h>
+#endif
+
+TEST_CASE("doctest test run owns its temporary memory") {
+    INFO("Create active INFO context storage");
+    static_cast<void>(doctest::toString(42));
+    CHECK(true);
+}
+
+int main(int argc, char **argv) {
+#if defined(_MSC_VER) && defined(_DEBUG)
+    {
+        // This used to own a thread-local stream for the process lifetime.
+        doctest::ContextOptions options{};
+        doctest::detail::DebugOutputWindowReporter reporter{options};
+    }
+#endif
+
+    int result = EXIT_SUCCESS;
+    {
+        doctest::Context context{argc, argv};
+        context.setOption("quiet", true);
+        context.setOption("no-debug-output", true);
+        result = context.run();
+    }
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+    if (_CrtDumpMemoryLeaks())
+        return EXIT_FAILURE;
+#endif
+
+    return result;
+}
